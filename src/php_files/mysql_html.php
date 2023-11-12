4e7321a189d83dc5807589a659e620ac72bce201
secripite <keonho04@gmail.com>
2023-11-12 22:30:06 +0900

    php

--- src/php_files/mysql_html.php
+++ src/php_files/mysql_html.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost:3306";
-$username = "root";
-$password = "nimda01384@";
-$dbname = "recommend_info";
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "database";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -13,42 +13,42 @@ if ($conn->connect_error) {
 }
 
 // Select data from book_info_table
-$sql = "SELECT * FROM book_info_table";
+$sql = "SELECT title, author, literature_OX, tag1, tag2 FROM book_info_table";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["other_column"]. "<br>";
+    echo "Title: " . $row["title"]. " - Author: " . $row["author"]. " - Literature: " . $row["literature_OX"]. " - Tag1: " . $row["tag1"]. " - Tag2: " . $row["tag2"]. "<br>";
   }
 } else {
-  echo "0 results";
+  echo "No results in book_info_table";
 }
 
 // Select data from exercise_info_table
-$sql = "SELECT * FROM exercise_info_table";
+$sql = "SELECT name, part FROM exercise_info_table";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["other_column"]. "<br>";
+    echo "Name: " . $row["name"]. " - Part: " . $row["part"]. "<br>";
   }
 } else {
-  echo "0 results";
+  echo "No results in exercise_info_table";
 }
 
 // Select data from certificate_table
-$sql = "SELECT * FROM certificate_table";
+$sql = "SELECT name, target, field FROM certificate_table";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["other_column"]. "<br>";
+    echo "Name: " . $row["name"]. " - Target: " . $row["target"]. " - Field: " . $row["field"]. "<br>";
   }
 } else {
-  echo "0 results";
+  echo "No results in certificate_table";
 }
 
 $conn->close();
